15bbd79315a8abf315c25bb3786d2d362e01282c
Iman Hedeshy <iman.hedeshy@gmail.com>
2023-09-04 17:13:22 -0400

    add chat

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const server = http.createServer(app);
 const path = require("path");
 
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Allow all origins or specify allowed origins like "http://localhost:3000"
+    methods: ["GET", "POST"],
+  },
+});
 
 require("dotenv").config();
 
@@ -32,7 +37,17 @@ app
   });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    socket.broadcast.emit("chat message", msg);
+  });
 });
 
 app.use("/users", userRoutes);
@@ -42,6 +57,6 @@ app.use("/threads", threadRoutes);
 const secretKey = generateSecretKey();
 console.log(secretKey);*/
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`The BOX server is running on port ${PORT}`);
 });
