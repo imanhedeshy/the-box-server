67b83063c6ad51e47529aea0b9620e20d57f757f
Iman Hedeshy <iman.hedeshy@gmail.com>
2023-10-15 22:16:12 -0400

    fix socketHandler path name bug

--- sockets/socketHandler.js
+++ sockets/socketHandler.js
@@ -1,32 +1,33 @@
 // sockets/socketHandler.js
 
 module.exports = (io, knex) => {
-  io.on("connection", async (socket) => {
-    // Broadcast chat history on connection
-    await knex("messages")
-      .then((data) => {
-        socket.emit("chat history", data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    socket.on("chat message", async (data) => {
-      console.log("message: " + data.message);
-      io.emit("chat message", data);
-      // Save message to database
+    io.on("connection", async (socket) => {
+      // Broadcast chat history on connection
       await knex("messages")
-        .insert({
-          name: data.username,
-          message: data.message,
-          timestamp: new Date(),
+        .then((data) => {
+          socket.emit("chat history", data);
         })
-        .then(() => {
-          console.log("Message saved to database");
-        })
-        .catch((error) => {
-          console.log(error);
+        .catch((err) => {
+          console.log(err);
         });
+  
+      socket.on("chat message", async (data) => {
+        console.log("message: " + data.message);
+        io.emit("chat message", data);
+        // Save message to database
+        await knex("messages")
+          .insert({
+            name: data.username,
+            message: data.message,
+            timestamp: new Date(),
+          })
+          .then(() => {
+            console.log("Message saved to database");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      });
     });
-  });
-};
+  };
+  
